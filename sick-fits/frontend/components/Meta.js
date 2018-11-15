314c0929b7ac620bba1a76334a0fa159a6c515ca
rizzyc <rizzyc@gmail.com>
2018-11-15 15:43:07 -0500

    styled page with Styled Components

--- sick-fits/frontend/components/Meta.js
+++ sick-fits/frontend/components/Meta.js
@@ -2,11 +2,11 @@ import Head from "next/head";
 
 const Meta = () => (
   <Head>
-    <meta charset="UTF-8" />
+    <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" href="/static/favicon.png" />
     <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
     <title>Sick Fits</title>
   </Head>
 );
