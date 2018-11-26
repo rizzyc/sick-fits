1aaff96d088418cb49c222e5d47d0a43efb35008
rizzyc <rizzyc@gmail.com>
2018-11-26 16:55:57 -0500

    creating queries mutations for yoga

---
+++ sick-fits/backend/src/createServer.js
@@ -0,0 +1,22 @@
+const { GraphQLServer } = require("graphql-yoga");
+const Mutation = require("./resolvers/Mutation");
+const Query = require("./resolvers/Query");
+const db = require("./db");
+
+// Creat the GraphQL Yoga Server
+
+function createSever() {
+  return new GraphQLServer({
+    typeDefs: "src/schema.graphql",
+    resolvers: {
+      Mutation,
+      Query
+    },
+    resolverValidationOptions: {
+      requireResolversForResolveType: false
+    },
+    context: req => ({ ...req, db })
+  });
+}
+
+module.exports = createSever;
--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+// TODO use express middleware to handle cookes (JWT)
+// TODO uese epress middleware to produce current isseus;
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server is now runing on port http://localhost:${deets.port}
+      `);
+  }
+);
